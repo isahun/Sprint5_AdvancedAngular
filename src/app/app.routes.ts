e6f77870348e900ff8a84a84b01e947f091c5c8f
Irene <isahun7@gmail.com>
2026-04-12 12:08:14 +0200

    fix: update imports and remove duplicate empty-path route in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { BookList } from './components/book-list/book-list';
+import { BookList } from './pages/book-list/book-list';
 import { BookDetails } from './pages/book-details/book-details';
-import { NotFound } from './components/not-found/not-found';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'books', component: BookList },
   { path: 'books/:bookId', component: BookDetails },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: NotFound }
 ];
