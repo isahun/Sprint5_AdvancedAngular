009b21907750feff23503bce9ec81079bc623f48
Irene <isahun7@gmail.com>
2026-04-13 19:31:46 +0200

    feat: add global error listeners and component input binding to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
