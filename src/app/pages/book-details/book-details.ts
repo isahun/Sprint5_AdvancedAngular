a38c8d81fa7b1013015cdf6f22252cf0a55f610c
Irene <isahun7@gmail.com>
2026-04-16 12:16:30 +0200

    feat: find book by id from HTTP data in BookDetails

--- src/app/pages/book-details/book-details.ts
+++ src/app/pages/book-details/book-details.ts
@@ -1,6 +1,8 @@
 import { Component, inject, input, computed } from '@angular/core';
 import { Router } from '@angular/router';
 import { BooksService } from '../../services/books-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 
 @Component({
   selector: 'app-book-details',
@@ -15,7 +17,9 @@ export class BookDetails {
 
   bookId = input.required<string>();
 
-  book = computed(() => this.service.getBook(this.bookId()));
+  books = toSignal(this.service.getBooks(), { initialValue: [] });
+
+  book = computed(() => this.books().find(b => b.id === this.bookId()));
 
   goBack() {
     this.router.navigate(['/books']);
