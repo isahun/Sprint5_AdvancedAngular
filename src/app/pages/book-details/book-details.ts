b13a5f9eab8404f5cf2d67c44b6ae79c571ae43b
Irene <isahun7@gmail.com>
2026-04-16 17:57:44 +0200

    feat: add bookUpdated output and onBookSaved handler to BookDetails

--- src/app/pages/book-details/book-details.ts
+++ src/app/pages/book-details/book-details.ts
@@ -1,27 +1,32 @@
-import { Component, inject, input, computed } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject, input, computed, output } from '@angular/core';
 import { BooksService } from '../../services/books-service';
 import { toSignal } from '@angular/core/rxjs-interop';
-
+import { BookEditComponent } from '../../components/book-edit-component/book-edit-component';
+import { Book } from '../../interfaces/book.interface';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
+  imports: [BookEditComponent],
   templateUrl: './book-details.html',
   styleUrl: './book-details.css',
 })
 export class BookDetails {
-
   service = inject(BooksService);
-  router  = inject(Router);
 
   bookId = input.required<string>();
+  bookDeleted = output<string>();
+  bookUpdated = output<Book>();
+  goBack = output<void>();
 
   books = toSignal(this.service.getBooks(), { initialValue: [] });
 
-  book = computed(() => this.books().find(b => b.id === this.bookId()));
+  book = computed(() => this.books().find((b) => b.id === this.bookId()));
 
-  goBack() {
-    this.router.navigate(['/books']);
+  onBookSaved(book: Book) {
+    this.bookUpdated.emit(book);
+  }
+
+  deleteBook() {
+    this.bookDeleted.emit(this.bookId());
   }
 }
