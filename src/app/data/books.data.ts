a8dc58f8ffd67fbac477bd68f2a5df85fcbfeb5f
Irene Virgínia <isahun7@gmail.com>
2026-04-19 01:49:29 +0200

    feat: extract default books data to data/books.data.ts

---
+++ src/app/data/books.data.ts
@@ -0,0 +1,7 @@
+import { Book } from '../interfaces/book.interface';
+
+export const DEFAULT_BOOKS: Book[] = [
+  { id: '1', title: 'The lord of the Rings', author: 'J.R.R. Tolkien', category: 'Epic' },
+  { id: '2', title: 'Metafísica de los tubos', author: 'Amélie Nothomb', category: 'Other' },
+  { id: '3', title: 'Poesía completa', author: 'Alejandra Pizarnik', category: 'Poetry' }
+];
