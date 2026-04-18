ace6026a6a1d03acf6880db7fd28683c5b17032a
Irene Virgínia <isahun7@gmail.com>
2026-04-19 01:28:46 +0200

    feat: add theme toggle button to Layout

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,5 +1,6 @@
-import { Component, ChangeDetectionStrategy  } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject  } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-layout',
@@ -8,4 +9,6 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Layout {}
+export class Layout {
+  themeService = inject(ThemeService);
+}
