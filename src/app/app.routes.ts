e610d481b54f35b1cca61593307e22d348a9c971
Irene Virgínia <isahun7@gmail.com>
2026-04-19 22:52:10 +0200

    feat: protect routes with authGuard and add admin and unauthorized routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,30 @@ import { BookDetails } from './pages/book-details/book-details';
 import { NotFound } from './pages/not-found/not-found';
 import { Layout } from './layout/layout';
 import { Login } from './components/login/login';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: '', component: Layout,
+  {
+    path: '',
+    component: Layout,
+    canActivate: [authGuard],
     children: [
       { path: '', component: Home },
       { path: 'books', component: BookList },
       { path: 'books/:bookId', component: BookDetails },
-    ]
-   },
-  { path: '**', component: NotFound }
+      {
+        path: 'admin',
+        loadComponent: () => import('./pages/admin/admin').then((m) => m.Admin),
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+    ],
+  },
+  {
+    path: 'unauthorized',
+    loadComponent: () =>
+      import('./pages/unauthorized/unauthorized').then((m) => m.Unauthorized),
+  },
+  { path: '**', component: NotFound },
 ];
